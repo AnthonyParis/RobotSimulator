17118baf24a459be1294aa685af5bdb802e01387
Pythony <contact@pythony.dev>
2020-12-13 14:21:47 +0100

    Grid

--- src/Robot.java
+++ src/Robot.java
@@ -3,6 +3,10 @@ public class Robot {
 
 	public static void main(String[] args) {
 		System.out.print("Hello World !");
+		
+		int[] robot = {0, 0};
+		
+		System.out.print("Robot : (" + robot[0] + "; " + robot[1] + ")");
 	}
 
 }
