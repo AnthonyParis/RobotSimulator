e99f1f4cd52324d5cb2ecc142bdef2df8df0aa66
Pythony <contact@pythony.dev>
2020-12-13 19:07:30 +0100

    Path

--- src/Robot.java
+++ src/Robot.java
@@ -5,45 +5,57 @@ public class Robot {
 		System.out.println("Hello World !");
 		
 		int[] robot = {0, 0, 0};
-		String path = "A";
+		String path = "LRRLA";
 
+		for(int i = 0; i < 20; i ++) {
+			if(Math.random() * 3 < 1) path += "L";
+			else if(Math.random() * 2 < 1) path += "R";
+			else path += "A";
+		}
+		
+		System.out.println("The path is : " + path);
+		
 		showRobot(robot);
 		
-		if(robot[2] == 0) {
-			if(path == "L") {
-				robot[0] --;
-				robot[2] = 3;
-			} else if(path == "R") {
-				robot[0] ++;
-				robot[2] = 1;
-			} else if(path == "A") robot[1] ++;
-		} else if(robot[2] == 1) {
-			if(path == "L") {
-				robot[1] ++;
-				robot[2] = 0;
-			} else if(path == "R") {
-				robot[1] --;
-				robot[2] = 2;
-			} else if(path == "A") robot[0] ++;
-		} else if(robot[2] == 2) {
-			if(path == "L") {
-				robot[0] ++;
-				robot[2] = 1;
-			} else if(path == "R") {
-				robot[0] --;
-				robot[2] = 3;
-			} else if(path == "A") robot[1] --;
-		} else if(robot[2] == 3) {
-			if(path == "L") {
-				robot[1] --;
-				robot[2] = 2;
-			} else if(path == "R") {
-				robot[1] ++;
-				robot[2] = 0;
-			} else if(path == "A") robot[0] --;
+		for(int i = 0; i < path.length(); i ++) {
+			char current = path.charAt(i);
+			
+			if(robot[2] == 0) {
+				if(current == 'L') {
+					robot[0] --;
+					robot[2] = 3;
+				} else if(current == 'R') {
+					robot[0] ++;
+					robot[2] = 1;
+				} else if(current == 'A') robot[1] ++;
+			} else if(robot[2] == 1) {
+				if(current == 'L') {
+					robot[1] ++;
+					robot[2] = 0;
+				} else if(current == 'R') {
+					robot[1] --;
+					robot[2] = 2;
+				} else if(current == 'A') robot[0] ++;
+			} else if(robot[2] == 2) {
+				if(current == 'L') {
+					robot[0] ++;
+					robot[2] = 1;
+				} else if(current == 'R') {
+					robot[0] --;
+					robot[2] = 3;
+				} else if(current == 'A') robot[1] --;
+			} else if(robot[2] == 3) {
+				if(current == 'L') {
+					robot[1] --;
+					robot[2] = 2;
+				} else if(current == 'R') {
+					robot[1] ++;
+					robot[2] = 0;
+				} else if(current == 'A') robot[0] --;
+			}
+	
+			showRobot(robot);
 		}
-
-		showRobot(robot);
 	}
 	
 	private static void showRobot(int[] robot) {
@@ -53,7 +65,7 @@ public class Robot {
 		else if(robot[2] == 2) facing = "South";
 		else if(robot[2] == 3) facing = "West";
 
-		System.out.println("Robot : (" + robot[0] + "; " + robot[1] + " - " + facing + ")");
+		System.out.println("Robot : (" + robot[0] + "; " + robot[1] + "; " + facing + ")");
 	}
 
 }
